3935d6afebf2886ee6b205694f1c9cf10a1163fe
Kai Krutscho <kkrutscho@TGH.local>
2020-02-24 16:58:00 +0100

    Added some basic stuff

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { ErrorMapper } from "utils/ErrorMapper";
 
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
+
+console.log("New script loaded");
 export const loop = ErrorMapper.wrapLoop(() => {
-  console.log(`Current game tick is ${Game.time}`);
+  // console.log(`Current game tick is ${Game.time}`);
 
   // Automatically delete memory of missing creeps
   for (const name in Memory.creeps) {
@@ -11,4 +13,6 @@ export const loop = ErrorMapper.wrapLoop(() => {
       delete Memory.creeps[name];
     }
   }
+  const roomName = Game.spawns.Spawn1.room.name;
+  console.log(`${roomName}| Game-tick: ${Game.time}` + "| CPU Used: " + Game.cpu.getUsed());
 });
